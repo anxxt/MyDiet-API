30a162e48913776ff69e22177408931ccbbcb97e
anxxt <antonio.garcia-garcia@outlook.com>
2024-04-07 00:40:42 +0200

    add status for correct diet

--- controllers/diets.js
+++ controllers/diets.js
@@ -6,7 +6,7 @@ export class DietController {
   getByName = async (req, res) => {
     const { name } = req.params
     const diet = await this.dietModel.getByName({ name })
-    if (diet) return res.json(diet)
+    if (diet) return res.status(200).json(diet)
 
     res.status(404).json({ message: 'Diet not found' })
   }
